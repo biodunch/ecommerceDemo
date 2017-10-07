2dc53c3ff9e09128a1e2af8477b6ef4898101936
biodun <biodunch@gmail.com>
2017-10-07 16:18:13 +0100

    create demo for wisov

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wisov Optika Demo' });
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('product', { title: 'Wisov Optika Demo' });
 });
 
 module.exports = router;
